18e7ad268426cd7404556de9260837d40b9bad33
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-14 17:50:07 +0700

    Add project files.

---
+++ grpc-client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grpc_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
